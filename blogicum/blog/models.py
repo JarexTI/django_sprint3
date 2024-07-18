fc2a04b7d7b05d0a1975c886c2ee026102689fdd
Viktor Gunba <gunba.viktor@gmail.com>
2024-07-19 01:46:51 +0300

    Updated the view functions in the blog app. The fields in the models are annotated.

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,49 +1,51 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
+from django.db.models import (CASCADE, SET_NULL, BooleanField, CharField,
+                              DateTimeField, ForeignKey, SlugField, TextField)
 
 User = get_user_model()
 
 
 class PublishedAndCreatedAt(models.Model):
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_published: BooleanField = BooleanField(default=True)
+    created_at: DateTimeField = DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
 
 
 class Category(PublishedAndCreatedAt):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
+    title: CharField = CharField(max_length=256)
+    description: TextField = TextField()
+    slug: SlugField = SlugField(unique=True)
 
     class Mate:
         abstract = True
 
 
 class Location(PublishedAndCreatedAt):
-    name = models.CharField(max_length=256)
+    name: CharField = CharField(max_length=256)
 
     class Mate:
         abstract = True
 
 
 class Post(PublishedAndCreatedAt):
-    title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField()
-    author = models.ForeignKey(
+    title: CharField = CharField(max_length=256)
+    text: TextField = TextField()
+    pub_date: DateTimeField = DateTimeField()
+    author: ForeignKey = ForeignKey(
         User,
-        on_delete=models.CASCADE
+        on_delete=CASCADE
     )
-    location = models.ForeignKey(
+    location: ForeignKey = ForeignKey(
         Location,
-        on_delete=models.SET_NULL,
+        on_delete=SET_NULL,
         blank=True,
         null=True)
-    category = models.ForeignKey(
+    category: ForeignKey = ForeignKey(
         Category,
-        on_delete=models.SET_NULL,
+        on_delete=SET_NULL,
         null=True
     )
 
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,26 +1,53 @@
+from datetime import date
 from typing import Union
-from blog.models import Post, Category, Location
 
+from blog.models import Category, Post
 from django.http import Http404, HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 
 def index(request: HttpRequest) -> HttpResponse:
     template: str = 'blog/index.html'
-    posts = Post.objects.filter(is_published=True)
-    context = {'post': posts}
+
+    post_list: list[Post] = get_list_or_404(
+        Post,
+        pub_date__lte=date.today(),
+        is_published=True,
+        category__is_published=True
+    )
+
+    context: dict[str, list] = {'post_list': post_list}
     return render(request, template, context)
 
 
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     template: str = 'blog/detail.html'
-    if pk not in POSTS_BY_ID:
+
+    post: Post = get_object_or_404(Post, pk=pk)
+    if (not post.is_published
+        or not post.category.is_published
+            or not post.pub_date.date() <= date.today()):
         raise Http404('Ошибка 404')
-    context: dict[str, POST] = {'post': POSTS_BY_ID[pk]}
+
+    context: dict[str, Post] = {'post': post}
     return render(request, template, context)
 
 
 def category_posts(request: HttpRequest, category_slug: str) -> HttpResponse:
     template: str = 'blog/category.html'
-    context: dict[str, str] = {'category_slug': category_slug}
+    post_list: list[Post] = get_list_or_404(
+        Post,
+        is_published=True,
+        pub_date__lte=date.today(),
+        category__slug=category_slug
+    )
+
+    category: Category = get_object_or_404(Category, slug=category_slug)
+    if not category.is_published:
+        raise Http404('Ошибка 404')
+
+    context: dict[str, Union[list[Post], Category]] = {
+        'post_list': post_list,
+        'category': category
+    }
     return render(request, template, context)
