6b9a1419b0bd496e24ccac8eb457e9ac568dba4e
Viktor Gunba <gunba.viktor@gmail.com>
2024-07-19 16:30:54 +0300

    Setting up the admin area.

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -7,47 +7,68 @@ User = get_user_model()
 
 
 class PublishedAndCreatedAt(models.Model):
-    is_published: BooleanField = BooleanField(default=True)
-    created_at: DateTimeField = DateTimeField(auto_now_add=True)
+    is_published: BooleanField = BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at: DateTimeField = DateTimeField(auto_now_add=True,
+                                              verbose_name='Добавлено')
 
     class Meta:
         abstract = True
 
 
 class Category(PublishedAndCreatedAt):
-    title: CharField = CharField(max_length=256)
-    description: TextField = TextField()
-    slug: SlugField = SlugField(unique=True)
+    title: CharField = CharField(max_length=256, verbose_name='Заголовок')
+    description: TextField = TextField(verbose_name='Описание')
+    slug: SlugField = SlugField(
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text=('Идентификатор страницы для URL; разрешены символы '
+                   'латиницы, цифры, дефис и подчёркивание.')
+    )
 
-    class Mate:
-        abstract = True
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 
 class Location(PublishedAndCreatedAt):
-    name: CharField = CharField(max_length=256)
+    name: CharField = CharField(max_length=256, verbose_name='Название места')
 
-    class Mate:
-        abstract = True
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
 
 
 class Post(PublishedAndCreatedAt):
-    title: CharField = CharField(max_length=256)
-    text: TextField = TextField()
-    pub_date: DateTimeField = DateTimeField()
+    title: CharField = CharField(max_length=256, verbose_name='Заголовок')
+    text: TextField = TextField(verbose_name='Текст')
+    pub_date: DateTimeField = DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text=('Если установить дату и время в будущем '
+                   '— можно делать отложенные публикации.')
+    )
     author: ForeignKey = ForeignKey(
         User,
-        on_delete=CASCADE
+        on_delete=CASCADE,
+        verbose_name='Автор публикации'
     )
     location: ForeignKey = ForeignKey(
         Location,
         on_delete=SET_NULL,
         blank=True,
-        null=True)
+        null=True,
+        verbose_name='Местоположение'
+    )
     category: ForeignKey = ForeignKey(
         Category,
         on_delete=SET_NULL,
-        null=True
+        null=True,
+        verbose_name='Категория'
     )
 
-    class Mate:
-        abstract = True
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,6 +2,7 @@ from datetime import date
 from typing import Union
 
 from blog.models import Category, Post
+from django.db.models import QuerySet
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
@@ -9,24 +10,22 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render
 def index(request: HttpRequest) -> HttpResponse:
     template: str = 'blog/index.html'
 
-    post_list: list[Post] = get_list_or_404(
-        Post,
+    post_list: QuerySet = Post.objects.filter(
         pub_date__lte=date.today(),
         is_published=True,
         category__is_published=True
-    )
+    )[0:5]
 
-    context: dict[str, list] = {'post_list': post_list}
+    context: dict[str, QuerySet] = {'post_list': post_list}
     return render(request, template, context)
 
 
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     template: str = 'blog/detail.html'
-
     post: Post = get_object_or_404(Post, pk=pk)
-    if (not post.is_published
-        or not post.category.is_published
-            or not post.pub_date.date() <= date.today()):
+    if (post.pub_date.date() > date.today()
+            or not post.is_published
+            or not post.category.is_published):
         raise Http404('Ошибка 404')
 
     context: dict[str, Post] = {'post': post}
@@ -37,12 +36,15 @@ def category_posts(request: HttpRequest, category_slug: str) -> HttpResponse:
     template: str = 'blog/category.html'
     post_list: list[Post] = get_list_or_404(
         Post,
+        category__slug=category_slug,
         is_published=True,
-        pub_date__lte=date.today(),
-        category__slug=category_slug
+        pub_date__lte=date.today()
     )
 
-    category: Category = get_object_or_404(Category, slug=category_slug)
+    category: Category = get_object_or_404(
+        Category,
+        slug=category_slug
+    )
     if not category.is_published:
         raise Http404('Ошибка 404')
 
